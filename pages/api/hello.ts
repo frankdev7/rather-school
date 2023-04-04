3f4dfbf48dadb19d220d3d123c85de233ed9ddc3
frankdev7 <franklyncr12@gmail.com>
2023-04-04 12:06:01 -0500

    rooms, students and relationships

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { Room } from '@/types';
 import type { NextApiRequest, NextApiResponse } from 'next'
+import * as Rooms from './rooms.json';
 
 type Data = {
   name: string
@@ -11,3 +13,18 @@ export default function handler(
 ) {
   res.status(200).json({ name: 'John Doe' })
 }
+
+export const getRooms = async (): Promise<
+  Room[]
+> => {
+  let rooms: Room[] = [];
+  Rooms.forEach(room => {
+    rooms.push({
+      id: room.id,
+      name: room.name,
+      description: room.description
+    })
+  });
+
+  return rooms;
+};
